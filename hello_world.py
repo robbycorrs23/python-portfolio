a177f4fa9f5dc5a32f510b4d441d4bdae64f2395
robbycorrs23 <jhello7@pm.me>
2024-02-29 05:04:36 +0000

    first project problem

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,6 @@
-#Hello Wolrd File
-print("Hello World!")
+weeks_in_a_year = 52
+current_weeks_remaining = int(input("What's your age?")) * weeks_in_a_year
+final_age = 90 * weeks_in_a_year
+
+weeks_remaining = f"You have {final_age - current_weeks_remaining} weeks remaining!"
+print(weeks_remaining)
